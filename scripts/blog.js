e330c04a66f639db7480c346d7f9b9cf949590e4
Nikhil M Warrier <70085759+nikhilmwarrier@users.noreply.github.com>
2021-01-01 15:14:22 +0530

    Add files via upload

--- scripts/blog.js
+++ scripts/blog.js
@@ -56,6 +56,15 @@ function share(text) {
       })
       .catch(console.error);
   } else {
-    console.log("not supported");
+    const el = document.createElement("textarea");
+    el.value = window.location.href;
+    el.setAttribute("readonly", "");
+    el.style.position = "absolute";
+    el.style.left = "-9999px";
+    document.body.appendChild(el);
+    el.select();
+    document.execCommand("copy");
+    document.body.removeChild(el);
+    alert("Link Copied");
   }
 }
