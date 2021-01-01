a40e3bc5337875540b0483bbe02bdc245908dc98
Nikhil M Warrier <70085759+nikhilmwarrier@users.noreply.github.com>
2021-01-01 14:08:17 +0530

    Add files via upload

--- scripts/common.js
+++ scripts/common.js
@@ -2,11 +2,43 @@ document.querySelector(".logo").addEventListener("click", () => {
   window.location.href = "./";
 });
 
-// dark-mode media query matched or not
-let matched = window.matchMedia("(prefers-color-scheme: dark)").matches;
+function themeInit() {
+  let theme = localStorage.getItem("theme");
 
-if (matched) {
-  document.body.classList.add("dark");
-} else {
-  document.body.classList.remove("dark");
+  if (theme == "dark") {
+    console.log("dark");
+    document.body.classList.add("dark");
+  } else if (theme == "light") {
+    document.body.classList.remove("dark");
+  } else {
+    // dark-mode media query matched or not
+    let matched = window.matchMedia("(prefers-color-scheme: dark)").matches;
+
+    if (matched) {
+      document.body.classList.add("dark");
+    } else {
+      document.body.classList.remove("dark");
+    }
+  }
+}
+
+themeInit();
+
+function toggleTheme() {
+  let toggleIcon = document.querySelector(".material-icons.theme-toggle-icon");
+  let theme = "";
+
+  document.body.classList.toggle("dark");
+  if (document.body.className == "dark") {
+    theme = "dark";
+  } else {
+    theme = "light";
+  }
+  if (theme == "light") {
+    toggleIcon.innerHTML = "dark_mode";
+    localStorage.setItem("theme", "light");
+  } else if (theme == "dark") {
+    toggleIcon.innerHTML = "brightness_low";
+    localStorage.setItem("theme", "dark");
+  }
 }
