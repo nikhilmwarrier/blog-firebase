cb009e25b8f76fe8b2db1f6b2644a54a9f22d6ff
Nikhil M Warrier <70085759+nikhilmwarrier@users.noreply.github.com>
2021-01-01 13:29:50 +0530

    Add files via upload

--- scripts/common.js
+++ scripts/common.js
@@ -7,7 +7,6 @@ let matched = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
 if (matched) {
   document.body.classList.add("dark");
-  alert("dark mode enabled");
 } else {
   document.body.classList.remove("dark");
 }
